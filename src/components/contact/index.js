3536c45416cabd2c2f81793797960f314f0d3bbd
TomTom2019 <daless2000be@yandex.com>
2022-10-16 19:54:49 +0200

    in progress

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,5 +1,25 @@
+import { useFormik } from "formik";
+import * as Yup from 'yup'
+import { Alert } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+
 
 const Contact = () =>{
+  const dispatch = useDispatch();
+  const formik = useFormik({
+  	initialValues:{email:'',firstname:'',lastname:'',message:''},
+  	validationSchema: Yup.object({
+  		email: Yup.string()
+  		.required('The email is required')
+  		.email('The email is invalied'),
+  		firstname: Yup.string()
+  		.required('firstname is required')
+
+  	})
+  })
+
+
+
 	return(
 		<>
           contact
