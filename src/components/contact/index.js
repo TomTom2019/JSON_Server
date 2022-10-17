0e369dc1f97aa1b5e0d4de82f7d1143542125cf1
TomTom2019 <daless2000be@yandex.com>
2022-10-17 14:44:28 +0200

    in progress

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -13,16 +13,79 @@ const Contact = () =>{
   		.required('The email is required')
   		.email('The email is invalied'),
   		firstname: Yup.string()
-  		.required('firstname is required')
+  		.required('firstname is required'),
+  		lastname:Yup.string()
+  		.required('last name is required'),
+  			message:Yup.string()
+  		.required('message is required')
+  		.max(500,'you message is too long')
 
-  	})
+  	}),
+  	onSubmit:(values,{resetForm})=>{
+       ////
+       console.log(values)
+  	}
   })
 
 
 
 	return(
 		<>
-          contact
+		 <h1>Contact Us</h1>
+		 <form className="mt-3" onSubmit={formik.handleSubmit} >
+		 	  <div className="form-group">
+		 	  	  <label htmlFor="email">Email adress</label>
+		 	  	  <input
+                    type='email'
+                    className="form-control"
+                    name="email"
+                    placeholder="email@gmail.com"
+                    {...formik.getFieldProps('email')}
+		 	  	  />
+		 	  	  {formik.errors.email && formik.touched.email ?
+                     <Alert variant="danger">
+                     	{formik.errors.email}
+                     </Alert>
+
+		 	  	  	:null}
+		 	  </div>
+		 	   <div className="form-group mt-2">
+		 	  	  <label htmlFor="firstname">First name</label>
+		 	  	  <input
+                    type='text'
+                    className="form-control"
+                    name="firstname"
+                    placeholder="Enter your firstname"
+                    {...formik.getFieldProps('firstname')}
+		 	  	  />
+		 	  	  {formik.errors.firstname && formik.touched.firstname ?
+                     <Alert variant="danger">
+                     	{formik.errors.firstname}
+                     </Alert>
+
+		 	  	  	:null}
+		 	  </div>
+
+		 	  <div className="form-group mt-2">
+		 	  	  <label htmlFor="lastname">Last name</label>
+		 	  	  <input
+                    type='text'
+                    className="form-control"
+                    name="lastname"
+                    placeholder="Enter your lastname"
+                    {...formik.getFieldProps('lastname')}
+		 	  	  />
+		 	  	  {formik.errors.lastname && formik.touched.lastname ?
+                     <Alert variant="danger">
+                     	{formik.errors.lastname}
+                     </Alert>
+
+		 	  	  	:null}
+		 	  </div>
+		 </form>
+
+
+          
 		</>
 		)
 }
